c0f6f0553678a6b87018d003ef975e153f800f9d
Dan <dan.laratta.jr@gmail.com>
2023-04-14 09:37:00 -0400

    user service class

--- src/main/java/com/jumpPlus/projects/repository/UserRepository.java
+++ src/main/java/com/jumpPlus/projects/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.jumpPlus.projects.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.jumpPlus.projects.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-	
+	Optional<User> findByEmail(String email);
+
 	
 }
--- src/main/java/com/jumpPlus/projects/service/UserService.java
+++ src/main/java/com/jumpPlus/projects/service/UserService.java
@@ -1,5 +1,48 @@
 package com.jumpPlus.projects.service;
 
-public class UserService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jumpPlus.projects.model.User;
+import com.jumpPlus.projects.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepository repo;
+
+	// GET user
+	public User getUserById(int id) {
+		
+		Optional<User> foundUser = repo.findById(id);
+		
+		if(!foundUser.isPresent()) {
+			// CREATE CUSTOM EXCEPTION
+			System.out.println("no user");
+		}
+		
+		return foundUser.get();
+	}
+	
+	
+	// CREATE user
+	public User createUser(User user) {
+		
+		Optional<User> userExists = repo.findByEmail(user.getEmail());
+		
+		if(userExists.isPresent()) {
+			// CREATE CUSTOM EXCEPTION
+			System.out.println("user already exists");
+		}
+		
+		return repo.save(user);
+	}
+	
+	
+	
+	
+	
 }
